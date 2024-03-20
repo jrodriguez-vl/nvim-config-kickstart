63602465807c9c17014858090e5861f395cfb8f0
Justin Rodriguez <jrod9520@gmail.com>
2024-03-20 19:01:08 +0900

    fix tab spacing

--- init.lua
+++ init.lua
@@ -316,7 +316,9 @@ require('lazy').setup({
 vim.o.hlsearch = false
 
 vim.o.tabstop = 4;
+vim.o.softtabstop = 4;
 vim.o.shiftwidth = 4;
+vim.o.expandtab = true;
 
 -- Make line numbers default
 vim.wo.number = true
