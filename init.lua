5b955860931423793d02e82eb368ebbaa1a853d5
Justin Rodriguez <jrod9520@gmail.com>
2024-03-21 22:32:41 +0900

    add php; harpoon; remove treesitter shortcuts

--- init.lua
+++ init.lua
@@ -270,7 +270,7 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   {
     'akinsho/toggleterm.nvim',
     version = "*",
@@ -485,7 +485,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'c_sharp', 'go', 'lua', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'vue', 'bash'},
+    ensure_installed = { 'c', 'cpp', 'c_sharp', 'go', 'lua', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'vue', 'php', 'bash'},
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = true,
@@ -496,60 +496,51 @@ vim.defer_fn(function()
     -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
     modules = {},
     highlight = { enable = true },
-    indent = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = '<c-space>',
-        node_incremental = '<c-space>',
-        scope_incremental = '<c-s>',
-        node_decremental = '<M-space>',
-      },
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ['aa'] = '@parameter.outer',
-          ['ia'] = '@parameter.inner',
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['ac'] = '@class.outer',
-          ['ic'] = '@class.inner',
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          [']m'] = '@function.outer',
-          [']]'] = '@class.outer',
-        },
-        goto_next_end = {
-          [']M'] = '@function.outer',
-          [']['] = '@class.outer',
-        },
-        goto_previous_start = {
-          ['[m'] = '@function.outer',
-          ['[['] = '@class.outer',
-        },
-        goto_previous_end = {
-          ['[M'] = '@function.outer',
-          ['[]'] = '@class.outer',
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ['<leader>a'] = '@parameter.inner',
-        },
-        swap_previous = {
-          ['<leader>A'] = '@parameter.inner',
-        },
-      },
-    },
+    -- indent = { enable = true },
+    -- incremental_selection = {
+    --   enable = true,
+    --   keymaps = {
+    --     init_selection = '<c-space>',
+    --     node_incremental = '<c-space>',
+    --     scope_incremental = '<c-s>',
+    --     node_decremental = '<M-space>',
+    --   },
+    -- },
+    -- textobjects = {
+    --   select = {
+    --     enable = true,
+    --     lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+    --     keymaps = {
+    --       -- You can use the capture groups defined in textobjects.scm
+    --       ['aa'] = '@parameter.outer',
+    --       ['ia'] = '@parameter.inner',
+    --       ['af'] = '@function.outer',
+    --       ['if'] = '@function.inner',
+    --       ['ac'] = '@class.outer',
+    --       ['ic'] = '@class.inner',
+    --     },
+    --   },
+    --   move = {
+    --     enable = true,
+    --     set_jumps = true, -- whether to set jumps in the jumplist
+    --     goto_next_start = {
+    --       [']m'] = '@function.outer',
+    --       [']]'] = '@class.outer',
+    --     },
+    --     goto_next_end = {
+    --       [']M'] = '@function.outer',
+    --       [']['] = '@class.outer',
+    --     },
+    --     goto_previous_start = {
+    --       ['[m'] = '@function.outer',
+    --       ['[['] = '@class.outer',
+    --     },
+    --     goto_previous_end = {
+    --       ['[M'] = '@function.outer',
+    --       ['[]'] = '@class.outer',
+    --     },
+    --   },
+    -- },
   }
 end, 0)
 
@@ -671,6 +662,7 @@ local servers = {
   html = { filetypes = { 'html', 'twig', 'hbs'} },
   angularls = {},
   cssls = {},
+  phpactor = {},
   vuels = {},
   omnisharp = {},
 }
