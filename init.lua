c886a65b31151710ef39253c6a119472a51bafab
Justin Rodriguez <jrod9520@gmail.com>
2024-06-10 01:57:11 +0900

    testintg another wsl push

--- init.lua
+++ init.lua
@@ -17,7 +17,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-
 local group_cdpwd = vim.api.nvim_create_augroup("group_cdpwd", { clear = true })
 vim.api.nvim_create_autocmd("VimEnter", {
   group = group_cdpwd,
