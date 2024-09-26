d74d7fc133fe1284f7b343820b3ffd787ebda7c6
Justin Rodriguez <jrod9520@gmail.com>
2024-09-26 16:45:00 +0900

    enable tree

--- init.lua
+++ init.lua
@@ -154,24 +154,24 @@ require('lazy').setup({
 
         -- Actions
         -- visual mode
-        map('v', '<leader>hs', function()
+        map('v', '<leader>Hs', function()
           gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
         end, { desc = 'stage git hunk' })
-        map('v', '<leader>hr', function()
+        map('v', '<leader>Hr', function()
           gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
         end, { desc = 'reset git hunk' })
         -- normal mode
-        map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
-        map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
-        map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
-        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
-        map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
-        map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
-        map('n', '<leader>hb', function()
+        map('n', '<leader>Hs', gs.stage_hunk, { desc = 'git stage hunk' })
+        map('n', '<leader>Hr', gs.reset_hunk, { desc = 'git reset hunk' })
+        map('n', '<leader>HS', gs.stage_buffer, { desc = 'git Stage buffer' })
+        map('n', '<leader>Hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
+        map('n', '<leader>HR', gs.reset_buffer, { desc = 'git Reset buffer' })
+        map('n', '<leader>Hp', gs.preview_hunk, { desc = 'preview git hunk' })
+        map('n', '<leader>Hb', function()
           gs.blame_line { full = false }
         end, { desc = 'git blame line' })
-        map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
-        map('n', '<leader>hD', function()
+        map('n', '<leader>Hd', gs.diffthis, { desc = 'git diff against index' })
+        map('n', '<leader>HD', function()
           gs.diffthis '~'
         end, { desc = 'git diff against last commit' })
 
@@ -184,43 +184,43 @@ require('lazy').setup({
       end,
     },
   },
-  {
-    -- Theme inspired by Atom
-    'catppuccin/nvim',
-    priority = 1000,
-    lazy = false,
-    -- opts = { transparent = true, },
-    config = function()
-      require('catppuccin').setup {
-        -- Set a style preset. 'dark' is default.
-        -- style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
-        transparent_background = true,
-        styles = {
-            -- sidebars = "transparent",
-            -- floats = "transparent",
-        },
-      }
-      require('catppuccin').load()
-    end,
-  },
   -- {
   --   -- Theme inspired by Atom
-  --   'navarasu/onedark.nvim',
+  --   'catppuccin/nvim',
   --   priority = 1000,
   --   lazy = false,
+  --   -- opts = { transparent = true, },
   --   config = function()
-  --     require('onedark').setup {
+  --     require('catppuccin').setup {
   --       -- Set a style preset. 'dark' is default.
-  --       style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
-  --       transparent = true,
+  --       -- style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
+  --       transparent_background = true,
   --       styles = {
   --           -- sidebars = "transparent",
   --           -- floats = "transparent",
   --       },
   --     }
-  --     require('onedark').load()
+  --     require('catppuccin').load()
   --   end,
   -- },
+  {
+    -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    lazy = false,
+    config = function()
+      require('onedark').setup {
+        -- Set a style preset. 'dark' is default.
+        style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
+        transparent = true,
+        styles = {
+            -- sidebars = "transparent",
+            -- floats = "transparent",
+        },
+      }
+      require('onedark').load()
+    end,
+  },
 
   {
     -- Set lualine as statusline
@@ -443,6 +443,48 @@ vim.api.nvim_create_autocmd("FileType", {
     end)
   end,
 })
+
+require('gitsigns').setup {
+  signs = {
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
+  },
+  signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+  numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  watch_gitdir = {
+    follow_files = true
+  },
+  auto_attach = true,
+  attach_to_untracked = false,
+  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+    ignore_whitespace = false,
+    virt_text_priority = 100,
+    use_focus = true,
+  },
+  current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+  sign_priority = 6,
+  update_debounce = 100,
+  status_formatter = nil, -- Use default
+  max_file_length = 40000, -- Disable if file is longer than this (in lines)
+  preview_config = {
+    -- Options passed to nvim_open_win
+    border = 'single',
+    style = 'minimal',
+    relative = 'cursor',
+    row = 0,
+    col = 1
+  },
+}
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
@@ -692,22 +734,29 @@ local on_attach = function(client, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  -- ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-}
+require('which-key').add({
+    { "<leader>c", group = "[C]ode" },
+    { "<leader>c_", hidden = true },
+    { "<leader>d", group = "[D]ocument" },
+    { "<leader>d_", hidden = true },
+    { "<leader>H", group = "[G]it Hunk" },
+    { "<leader>H_", hidden = true },
+    { "<leader>r", group = "[R]ename" },
+    { "<leader>r_", hidden = true },
+    { "<leader>s", group = "[S]earch" },
+    { "<leader>s_", hidden = true },
+    { "<leader>t", group = "[T]oggle" },
+    { "<leader>t_", hidden = true },
+    { "<leader>w", group = "[W]orkspace" },
+    { "<leader>w_", hidden = true },
+})
+
 -- register which-key VISUAL mode
 -- required for visual <leader>hs (hunk stage) to work
-require('which-key').register({
-  ['<leader>'] = { name = 'VISUAL <leader>' },
-  ['<leader>h'] = { 'Git [H]unk' },
-}, { mode = 'v' })
+require('which-key').add({
+    { "<leader>", group = "VISUAL <leader>", mode = "v" },
+    { "<leader>h", desc = "Git [H]unk", mode = "v" },
+})
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
@@ -736,7 +785,7 @@ local servers = {
   -- gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
-  tsserver = {},
+  -- tsserver = {},
   html = { filetypes = { 'html', 'twig', 'hbs'} },
   -- angularls = {},
   cssls = {},
