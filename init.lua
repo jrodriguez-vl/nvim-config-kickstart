3328848300a227aa18b1814c43d2e330a7467fed
Justin Rodriguez <jrod9520@gmail.com>
2025-02-19 19:09:16 +0900

    allow wsl to connect to godot

--- init.lua
+++ init.lua
@@ -877,6 +877,8 @@ local gdscript_config = {
 if vim.fn.has("win32") == 1 then
 	-- Windows specific. Requires nmap installed (`winget install nmap`)
 	gdscript_config["cmd"] = { "ncat", "localhost", os.getenv("GDScript_Port") or "6005" }
+else
+  gdscript_config["cmd"] = vim.lsp.rpc.connect("172.29.176.1", "6005")
 end
 require("lspconfig").gdscript.setup(gdscript_config)
 
