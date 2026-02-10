49d67aee4f44dc26c01b57f1617ab1c5236958e4
Justin <jrod9520@gmail.com>
2026-02-10 13:08:42 +0900

    neotree hide stuffs

--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -12,7 +12,15 @@ return {
         filesystem = {
             bind_to_cwd = false,
             follow_current_file = { enabled = true },
-            use_libuv_file_watcher = true
+            use_libuv_file_watcher = true,
+            filtered_items = {
+                hide_by_pattern = {
+                    "*.import",
+                    "*.tscn",
+                    "*.uid",
+                    "*.png",
+                }
+            }
         }
 
     },
